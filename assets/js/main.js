0b304dd37f3b54f13c6899962912cc38c5b551b2
Elena Murzina <gunjasha777@gmail.com>
2024-07-26 10:44:26 +0300

    add scroll+ end

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,16 +32,86 @@ const linkAction = () => {
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*=============== ADD BLUR HEADER ===============*/
+const blurHeader = () => {
+  const header = document.getElementById('header');
 
+  //Add a class if the bottom offset if greater than 50 50 of the viewport
+  this.scrollY >= 50 ? header.classList.add('blur-header')
+                     : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
 
 /*=============== SWIPER PLANETS ===============*/ 
+const swiperTravel = new Swiper('.travel__swiper', {
+  loop: true,
+  spaceBetween: '32',
+  grabCursor: true,
+  slidesPerView: 'auto',
+  centeredSlides: 'auto',
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  breakpoints: {
+    600:{
+      slidesPerView: 2,
+    },
+    900:{
+      slidesPerView: 3,
+    },
+  },
+});
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
-
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up');
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp)
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
 
+const scrollActive = () => {
+  const scrollDown = window.scrollY
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+          sectionTop = current.offsetTop - 58,
+          sectionId = current.getAttribute('id'),
+          sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+        
+    if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link');
+    }else {
+      sectionsClass.classList.remove('active-link');
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2500,
+  delay: 300,
+  //reset: true, // Animations repeat
+})
+
+sr.reveal(`.home__data, .travel__swiper, .contact__container`)
+sr.reveal(`.home__img`, {origin: 'bottom'})
+sr.reveal(`.home__ovni`, {delay: 800})
+sr.reveal(`.explore__img`, {origin: 'left'})
+sr.reveal(`.explore__data`, {origin: 'right'})
+sr.reveal(`.explore__planet`, {origin: 'right', delay: 800})
+sr.reveal(`.history__card`, {interval: 100})
+sr.reveal(`.history__planet-1`, {origin: 'left', delay: 1000})
+sr.reveal(`.history__planet-2`, {origin: 'right', delay: 1200})
+sr.reveal(`.footer_planet-1`, {origin: 'bottom', delay: 600})
+sr.reveal(`.footer_planet-2`, { delay: 800})
